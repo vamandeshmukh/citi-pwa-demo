cfe2acbffff602979173e42b8e4a57b8ece8afe2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-12 13:09:29 +0530

    pwa

--- src/server.js
+++ src/server.js
@@ -1,17 +1,12 @@
 
-console.log('app starting...');
+console.log('Citi PWA App is starting...');
 
 // import express from 'express';
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// app.use();
-
-app.get('', (req, res) => {
-    console.log('welcome');
-    res.send('welcome to Citi');
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`App is running at http://localhost:${port}`);
