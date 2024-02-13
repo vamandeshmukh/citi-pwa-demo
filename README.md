d822fccd772bf2d07dc603cb1d0f1c845592fb58
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-13 12:55:13 +0530

    pwa

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 https://docs.google.com/document/d/1G2pVNapB3e2mtJMJYOj3ib5jUPNn5brUyVBs1pt9Sg8/edit?usp=sharing
 
 
-case study 
+# case study 
 
 url : https://jsonplaceholder.typicode.com/ 
 
-Citi Blog App 
+## Citi Blog App 
 
 View list of blogs /posts 
 View blog details 
---
+++ citi-blog-app/script.js
@@ -0,0 +1,24 @@
+
+const blogUrl = 'https://jsonplaceholder.typicode.com/posts';
+const commentsUrl = 'https://jsonplaceholder.typicode.com/comments/';
+const writersUrl = 'https://jsonplaceholder.typicode.com/users/';
+
+// fetch all blogs list 
+
+fetch(blogUrl)
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        console.log(data);
+        let blogList = '';
+        data.forEach(element => {
+            blogList += `<a href="./blog-details.html?blog-id=${element.id}">${element.title}</a><br />`;
+        });
+        document.getElementById('blog-list').innerHTML = blogList;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
