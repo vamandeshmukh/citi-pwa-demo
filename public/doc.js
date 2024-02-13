92606b3c9df798b492289dc5cc2c856ee158cd74
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-13 11:15:26 +0530

    pwa

--- public/doc.js
+++ public/doc.js
@@ -2,7 +2,10 @@ fetch('manifest.json')
     .then(response => response.json())
     .then(data => {
         document.title = data.name;
+        document.getElementsByTagName('body')[0].style.background = data.background_color;
+        document.getElementsByTagName('body')[0].style.color = data.theme_color;
     })
     .catch(error => {
         console.error('Error fetching manifest.json:', error);
     });
+    
