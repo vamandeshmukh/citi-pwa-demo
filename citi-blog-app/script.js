fc261a6d51de7efad549d046a5477b19b961bf35
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-13 13:22:10 +0530

    pwa

--- citi-blog-app/script.js
+++ citi-blog-app/script.js
@@ -15,12 +15,31 @@ fetch(blogUrl)
         data.forEach(element => {
             blogList += `<a href="./blog-details.html?blog-id=${element.id}">${element.title}</a><br />`;
         });
-        document.getElementById('blog-list').innerHTML = blogList;
+        if (document.getElementById('blog-list'))
+            document.getElementById('blog-list').innerHTML = blogList;
     })
     .catch((error) => {
         console.log(error);
     });
 
+// fetch blog details 
+
+const blogId = new URLSearchParams(window.location.search).get('blog-id');
+
+fetch(`${blogUrl}/${blogId}`)
+    .then((resp) => {
+        return resp.json();
+    })
+    .then((data) => {
+        console.log(data);
+        if (document.getElementById('blog-title'))
+            document.getElementById('blog-title').innerText = data.title;
+        if (document.getElementById('blog-body'))
+            document.getElementById('blog-body').innerText = data.body;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
 
