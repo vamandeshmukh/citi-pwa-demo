0d64cfddaa7ea13b213cbf607b085bc19b27c3f6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-14 10:39:07 +0530

    pwa

---
+++ day-3-stuff/citi-pwa-2/serviceWorker.js
@@ -0,0 +1,40 @@
+const citiCache = "citi-app-v1";
+
+const assets = [
+  // "/",
+  // "/index.html",
+  // "/css/style.css",
+  // "/js/app.js",
+  // "/images/citi-logo.png"
+];
+
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//     caches.open(citiCache).then(cache => {
+//       cache.addAll(assets);
+//     }).then(() => {
+//       self.skipWaiting();
+//     })
+//   );
+// });
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(citiCache).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
+
+
+
+
---
+++ day-3-stuff/citi-pwa/js/app.js
@@ -0,0 +1,12 @@
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/serviceWorker.js", { scope: "/" })
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+const submitForm = () => {
+  console.log('sumitted');
+};
