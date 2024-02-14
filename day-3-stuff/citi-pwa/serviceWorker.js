13cf26781a845a45814807417ef45ec67d557acb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-14 12:05:46 +0530

    pwa

--- day-3-stuff/citi-pwa/serviceWorker.js
+++ day-3-stuff/citi-pwa/serviceWorker.js
@@ -4,19 +4,28 @@ const citiCache = "citi-app-v1";
 
 const assets = [
   // "/",
-  // "/index.html",
+  "/index.html",
+  "/page404.html",
   // "/css/style.css",
   // "/js/app.js",
   // "/images/citi-logo.png"
 ];
 
+async function precache() {
+  console.log('precache');
+  const cache = await caches.open(citiCache);
+  return cache.addAll(assets);
+}
+
 self.addEventListener("install", installEvent => {
+  console.log('install');
   installEvent.waitUntil(
-    caches.open(citiCache).then(cache => {
-      cache.addAll(assets);
-    }).then(() => {
-      self.skipWaiting();
-    })
+    precache()
+    // caches.open(citiCache).then(cache => {
+    //   cache.addAll(assets);
+    // }).then(() => {
+    //   self.skipWaiting();
+    // })
   );
 });
 
@@ -29,14 +38,88 @@ self.addEventListener("install", installEvent => {
 // });
 
 
+// self.addEventListener("fetch", fetchEvent => {
+//   fetchEvent.respondWith(
+//     caches.match(fetchEvent.request).then(res => {
+//       return res || fetch(fetchEvent.request);
+//     })
+//   );
+// });
+
 self.addEventListener("fetch", fetchEvent => {
+  console.log('1');
   fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
+    caches.match(fetchEvent.request)
+      .then(res => {
+        console.log(res);
+        return res || fetch(fetchEvent.request);
+      })
+      .catch((error) => {
+        console.log("error fetching", error);
+        // return new Response("Offline");
+        return caches.match("/page404.html")
+      })
   );
 });
 
+
+
+
+
+
+// const citiCache = "citi-app-v1";
+
+// const assets = [
+//   // "/",
+//   // "/index.html",
+//   "/page404.html",
+//   // "/css/style.css",
+//   // "/js/app.js",
+//   // "/images/citi-logo.png"
+// ];
+
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//     caches.open(citiCache).then(cache => {
+//       cache.addAll(assets);
+//     }).then(() => {
+//       self.skipWaiting();
+//     })
+//   );
+// });
+
+// // self.addEventListener("install", installEvent => {
+// //   installEvent.waitUntil(
+// //     caches.open(citiCache).then(cache => {
+// //       cache.addAll(assets);
+// //     })
+// //   );
+// // });
+
+
+// // self.addEventListener("fetch", fetchEvent => {
+// //   fetchEvent.respondWith(
+// //     caches.match(fetchEvent.request).then(res => {
+// //       return res || fetch(fetchEvent.request);
+// //     })
+// //   );
+// // });
+
+// self.addEventListener("fetch", fetchEvent => {
+//   console.log('1');
+//   fetchEvent.respondWith(
+//     caches.match(fetchEvent.request)
+//       .then(res => {
+//         console.log(res);
+//         return res || fetch(fetchEvent.request);
+//       })
+//       .catch((error) => {
+//         console.log("error fetching", error);
+//         // return new Response("Offline");
+//         return caches.match("/page404.html")
+//       })
+//   );
+// });
 
 
 
