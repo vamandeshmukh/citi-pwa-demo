9485ab0f608374f39f714faec18771dc44678c94
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-02-15 12:30:52 +0530

    pwa

--- day-3-stuff/citi-blog-pwa/js/app.js
+++ day-3-stuff/citi-blog-pwa/js/app.js
@@ -23,12 +23,14 @@ fetch(url)
   .catch();
 
 
+// camera access 
+
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then((stream) => {
       let video = document.getElementById('camera-preview');
-      video.srcObject = stream;
-      video.onplay();
+      // video.srcObject = stream;
+      // video.onplay();
     })
     .catch(err => console.log(err));
 }
@@ -36,7 +38,106 @@ else {
   console.log('not working!');
 }
 
+// source: https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API/Taking_still_photos 
 
+
+(() => {
+  const width = 320;
+  let height = 0;
+  let streaming = false;
+  let video = null;
+  let canvas = null;
+  let photo = null;
+  let startbutton = null;
+
+  function showViewLiveResultButton() {
+    if (window.self !== window.top) {
+      document.querySelector(".contentarea").remove();
+      const button = document.createElement("button");
+      button.textContent = "View live result of the example code above";
+      document.body.append(button);
+      button.addEventListener("click", () => window.open(location.href));
+      return true;
+    }
+    return false;
+  }
+
+  function startup() {
+    if (showViewLiveResultButton()) {
+      return;
+    }
+    video = document.getElementById("video");
+    canvas = document.getElementById("canvas");
+    photo = document.getElementById("photo");
+    startbutton = document.getElementById("startbutton");
+
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+
+    video.addEventListener(
+      "canplay",
+      (ev) => {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false,
+    );
+
+    startbutton.addEventListener(
+      "click",
+      (ev) => {
+        takepicture();
+        ev.preventDefault();
+      },
+      false,
+    );
+    clearphoto();
+  }
+
+  function clearphoto() {
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const data = canvas.toDataURL("image/png");
+    photo.setAttribute("src", data);
+  }
+
+  function takepicture() {
+    const context = canvas.getContext("2d");
+    if (width && height) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+
+      const data = canvas.toDataURL("image/png");
+      photo.setAttribute("src", data);
+    } else {
+      clearphoto();
+    }
+  }
+  window.addEventListener("load", startup, false);
+})();
+
+
+
+// notifications 
 self.addEventListener("load", () => {
   console.log('load');
   Notification.requestPermission().then((result) => {
@@ -56,7 +157,7 @@ const randomNotification = async () => {
     icon: notifImg,
   };
   new Notification(notifTitle, options);
-  setTimeout(randomNotification, 10000);
+  setTimeout(randomNotification, 1000000);
 }
 
 
